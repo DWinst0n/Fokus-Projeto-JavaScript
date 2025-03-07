e60bf33478b5090dfc43be2af4b3692e5cce3a9a
DWinst0n <daviwinstonsm@gmail.com>
2025-03-07 13:43:41 -0300

    Adição de alterações de texto e imagem com base no contexto

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const botoesContexto = document.querySelectorAll(".app__card-button");
+
+
 botoesContexto.forEach(botao => {
     botao.addEventListener("click", () => {
         document.querySelector(".active")?.classList.remove("active");
         botao.classList.add("active");
 
         let btnTxt = botao.textContent.trim().toLowerCase().split(" ");
-        let contexto = btnTxt.join("-")
+        let contexto = btnTxt.join("-");
         document.querySelector("html").dataset.contexto = contexto;
-
-        
-
+        document.querySelector(".app__image").src = `imagens/${contexto}.png`;
+        switch (botao.dataset.contexto) {
+            case "short":
+                document.querySelector(".app__title").innerHTML = `                Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+                break;
+            case "long":
+                document.querySelector(".app__title").innerHTML = `                Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+                break;
+            default:
+                document.querySelector(".app__title").innerHTML = `                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+                break;
+        }
     });
 });
 
+
+
 const botaoMusica = document.getElementById("alternar-musica");
 const musica = new Audio("sons/luna-rise-part-one.mp3");
-
 botaoMusica.addEventListener("click", () => {
     if (botaoMusica.checked) {
         musica.play();
